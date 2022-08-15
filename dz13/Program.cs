3f678a50965e2b7e19c8507053578cf0ff3524b2
Sergey <SergeyNesterov01@gmail.com>
2022-08-15 19:46:35 +0500

    seminar2

---
+++ dz13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// Решить без использования строк.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99)
+{
+    while (number > 999)
+    {
+    number = number / 10;
+    }
+int result = number % 10;
+Console.WriteLine($":{result}");
+}
+else Console.WriteLine ("третьей цифры нет");
---
+++ dz15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая
+// принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.WriteLine("Введите число :");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a == 1) System.Console.WriteLine("Нет");
+else if (a == 2) System.Console.WriteLine("Нет");
+else if (a == 3) System.Console.WriteLine("Нет");
+else if (a == 4) System.Console.WriteLine("Нет");
+else if (a == 5) System.Console.WriteLine("Нет");
+else if (a == 6) System.Console.WriteLine("Да");
+else if (a == 7) System.Console.WriteLine("Да");
+else System.Console.WriteLine("Число не в диапазоне 1-7");
